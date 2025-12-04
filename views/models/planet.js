350567323817c48ec1793d796a4aae3e1c71df79
gabogara <gabo1gara@gmail.com>
2025-12-04 15:57:31 -0800

    Add set up express server, planet model and .env config

---
+++ views/models/planet.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const planetSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  distanceFromSun: Number,
+  hasRings: Boolean,
+  image: String,
+});
+
+const Planet = mongoose.model("Planet", planetSchema);
+
+module.exports = Planet;
