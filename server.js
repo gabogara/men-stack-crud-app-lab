4377d349b80407ba88effc74e15ec08fd1c4b064
gabogara <gabo1gara@gmail.com>
2025-12-04 16:43:41 -0800

    add new planet form route and new.ejs view

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 
-// const Planet = require("./models/planet.js");
+const Planet = require("./models/planet.js");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+// GET /planets/new
+app.get("/planet/new", (req, res) => {
+  res.send("This route sends the user a form page!");
 });
 
 app.get("/", async (req, res) => {
